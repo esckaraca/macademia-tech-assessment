4611e7d29d67b57f31702c7d58815057c8798079
Ecem Karaca <esckaraca@gmail.com>
2023-02-23 23:51:18 +0300

    Added AWS and Cloudflare providers

---
+++ provider.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_providers {
+    cloudflare = {
+      source  = "cloudflare/cloudflare"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "aws" {
+    region = var.aws_region
+}
+
+provider "cloudflare" {}
